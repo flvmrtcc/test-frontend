df173c2051483e111ec2440e0c503e8311d46522
flvmrtcc <flavio.martucci.fm@gmail.com>
2024-01-31 20:40:07 +0100

    fetch users data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import UsersList from './components/UsersList';
-import availableUsersData from './data.json'; 
 
 export default function App() {
-  
+  const uri = "https://reqres.in/api/users";
+  const [availableUsersData, setAvailableUsersData] = useState([]);
+  const [checkedUsersData, setCheckedUsersData] = useState([]);
+
+  useEffect(() => {
+    fetch(uri)
+      .then(data => data.json())
+      .then(data => data.data)
+      .then(setAvailableUsersData)
+      .catch(console.error);
+  });
+
   return (
     <div className="App">
 
       <div className="flexcontainer">
         <UsersList listName={"Utenti disponibili"} usersData={availableUsersData} />
-        <UsersList listName={"Utenti controllati"} />
+        <UsersList listName={"Utenti controllati"} usersData={checkedUsersData} />
       </div>
 
     </div>
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,19 +1,9 @@
 import User from "./User";
 
 export default function UsersList({ listName = "Lista utenti", usersData = [] }) {
-    // const user = {
-    //     id: 1,
-    //     email: "george.bluth@reqres.in",
-    //     first_name: "George",
-    //     last_name: "Bluth",
-    //     avatar: "https://reqres.in/img/faces/1-image.jpg"
-    // }
-
     return (
         <div className="users-list">
             <h2>{listName}</h2>
-            {/* <h5>ID Nome Cognome Email</h5> */}
-
             {
                 usersData.length > 0 ?
                     usersData.map(user => (
@@ -22,7 +12,6 @@ export default function UsersList({ listName = "Lista utenti", usersData = [] })
                     :
                     <h4>Nessun utente presente!</h4>
             }
-            
         </div>
     )
 }
